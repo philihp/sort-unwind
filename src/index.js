0dc952ea1cd434038ee1784de1d35ada6733b8c8
Philihp Busby <philihp@gmail.com>
2021-03-10 08:51:12 +0000

    import only parts of ramda needed for file size

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import R from 'ramda'
+import {
+  uncurryN,
+  pipe,
+  addIndex,
+  map,
+  zip,
+  sortBy,
+  prop,
+  transpose,
+} from 'ramda'
 
-export default R.uncurryN(2, (rank) =>
-  R.pipe(
-    R.addIndex(R.map)((element, idx) => [element, idx]),
-    R.zip(rank),
-    R.sortBy(R.prop(0)),
-    R.map(R.prop(1)),
-    R.transpose
+export default uncurryN(2, (rank) =>
+  pipe(
+    addIndex(map)((element, idx) => [element, idx]),
+    zip(rank),
+    sortBy(prop(0)),
+    map(prop(1)),
+    transpose
   )
 )
