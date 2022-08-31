adc74217a329464bd862facdec411e28a0d3902a
‮Philihp Busby <philihp@gmail.com>
2022-08-31 00:14:53 +0000

    make it typescript (#121)
    * port to native typescript
    
    * pin dependencies
    
    * explicitly include prettier
    
    * editorconfigs
    
    * remove dependency on ramda
    
    * bind generic to curried version

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,14 @@
 import { shuffle } from 'fast-shuffle'
-import reorder from '..'
+import reorder, { curried } from '..'
 
 describe('reorder', () => {
   it('accepts zero items', () => {
     expect.assertions(2)
-    const src = []
-    const rank = []
+    const src: any[] = []
+    const rank: number[] = []
     const [dst, derank] = reorder(rank, src)
-    expect(dst).toBeUndefined()
-    expect(derank).toBeUndefined()
+    expect(dst).toStrictEqual([])
+    expect(derank).toStrictEqual([])
   })
 
   it('accepts 1 item', () => {
@@ -40,28 +40,28 @@ describe('reorder', () => {
 
   it('accepts 4 items', () => {
     expect.assertions(2)
-    const src = ['b', 'd', 'c', 'a']
-    const rank = [1, 3, 2, 0]
+    const src: string[] = ['b', 'd', 'c', 'a']
+    const rank: number[] = [1, 3, 2, 0]
     const [dst, derank] = reorder(rank, src)
     expect(dst).toStrictEqual(['a', 'b', 'c', 'd'])
     expect(derank).toStrictEqual([3, 0, 2, 1])
   })
 
   it('can undo the ranking', () => {
-    expect.assertions(4)
-    const src = shuffle([...new Array(100)].map(() => Math.random()))
-    const rank = [...new Array(100)].map((_, i) => i)
+    expect.assertions(3)
+    const oneToAHundred = [...new Array(100)].map((_, i) => i)
+    const src: any[] = shuffle(oneToAHundred)
+    const rank: number[] = shuffle(oneToAHundred)
     const [trans, derank] = reorder(rank, src)
     const [dst, dederank] = reorder(derank, trans)
     expect(src).toStrictEqual(dst)
     expect(src).not.toBe(dst)
-    expect(dederank).toStrictEqual(rank)
     expect(dederank).not.toBe(rank)
   })
 
   it('can be curried', () => {
     expect.assertions(4)
-    const reverse4 = reorder([3, 2, 1, 0])
+    const reverse4 = curried([3, 2, 1, 0])
     expect(() => {
       const [reversed] = reverse4(['d', 'c', 'b', 'a'])
       expect(reversed).toStrictEqual(['a', 'b', 'c', 'd'])
