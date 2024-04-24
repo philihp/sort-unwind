8c8dba0ae8df61dad81a5aa8433ab1d1b677266e
‮Philihp Busby <philihp@gmail.com>
2024-04-24 09:23:34 +0000

    update to eslint 9 config format (#490)
    * update to eslint 9 config format
    
    * turn off fail fast
    
    * derp
    
    * add eslint-config-prettier
    
    * bump lint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js'
+import jestPlugin from 'eslint-plugin-jest'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', 'eslint.config.js'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    extends: [eslint.configs.recommended, eslintPluginPrettierRecommended, ...tseslint.configs.recommended],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['src/**/__tests__/*.ts', 'src/**/*.test.ts'],
+    extends: [jestPlugin.configs['flat/recommended']],
+  }
+)
